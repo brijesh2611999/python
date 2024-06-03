2a2a0cad51d10c95c96c4d2dc689efa68889a7a8
Brijesh verma <bv2611999@gmail.com>
2024-06-03 09:31:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def man():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/', methods=['POST'])
 def index():
     text = request.form['review_text']
 
